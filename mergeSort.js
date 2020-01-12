273cae06668d2a0b5478158956e3b31c3bbe2249
rj33536 <rj33536@gmail.com>
2020-01-12 19:22:23 +0530

    Visualisation complete

--- mergeSort.js
+++ mergeSort.js
@@ -4,51 +4,80 @@ let arr = [];
 for (let i = 0; i < 100; i++) {
     arr.push(Math.floor(Math.random() * 50));
 }
-
+/*
 function merge(l, mid, r) {
-    let newArray = [];
-    let i = l,
-        j = mid + 1;
-    while (i <= mid && j <= r) {
-        if (arr[i] > arr[j]) {
-            newArray.push(arr[j]);
-            j++;
-        } else {
+    
+    i = 0;
+
+}*/
+let mergeSort = [
+    [0, arr.length - 1]
+];
+let merge = [];
+while (mergeSort.length != 0) {
+    console.log(mergeSort);
+    let mytop = mergeSort.pop();
+    if (mytop[0] != mytop[1]) {
+
+
+        let mid = Math.floor((mytop[0] + mytop[1]) / 2);
+        mergeSort.push([mytop[0], mid]);
+        mergeSort.push([mid + 1, mytop[1]]);
+        merge.push([mytop[0], mid, mytop[1]]);
+    }
+}
+var myInterval = setInterval(() => {
+
+
+    if (merge.length == 0) {
+        ctx.fillStyle = 'blue';
+        for (let itr = 0; itr < arr.length; itr++) {
+
+            ctx.fillRect(itr * 4 + 4, 0, 4, arr[itr] * 8);
+        }
+        clearInterval(myInterval);
+    } else {
+        mytop = merge.pop();
+        let l = mytop[0],
+            mid = mytop[1],
+            r = mytop[2];
+        let newArray = [];
+        let i = l,
+            j = mid + 1;
+        while (i <= mid && j <= r) {
+            if (arr[i] > arr[j]) {
+                newArray.push(arr[j]);
+                j++;
+            } else {
+                newArray.push(arr[i]);
+                i++;
+            }
+        }
+        while (i <= mid) {
             newArray.push(arr[i]);
             i++;
         }
-    }
-    while (i <= mid) {
-        newArray.push(arr[i]);
-        i++;
-    }
-    while (j <= r) {
-        newArray.push(arr[j]);
-        j++;
-    }
-    for (i = l; i <= r; i++)
-        arr[i] = newArray[i - l];
-    setTimeout(() => {
-        ctx.clearRect(0, 0, 400, 400);
-        ctx.fillStyle = 'blue';
-        for (let itr = 0; itr < arr.length; itr++) {
-            ctx.fillRect(itr * 4 + 4, 0, 4, arr[itr] * 8);
+        while (j <= r) {
+            newArray.push(arr[j]);
+            j++;
         }
-    }, 1);
-}
-
-function mergeSort(l, r) {
-    if (l == r)
-        return;
-
-    let mid = Math.floor((l + r) / 2);
+        for (i = l; i <= r; i++)
+            arr[i] = newArray[i - l];
+    }
 
 
+    ctx.clearRect(0, 0, 400, 400);
 
-    mergeSort(l, mid);
-    mergeSort(mid + 1, r);
-    merge(l, mid, r);
+    for (let itr = 0; itr < arr.length; itr++) {
+        ctx.fillStyle = 'blue';
+        if (itr >= mytop[0] & itr <= mytop[2])
+            ctx.fillStyle = 'yellow';
 
-}
-mergeSort(0, arr.length - 1);
+        ctx.fillRect(itr * 4 + 4, 0, 4, arr[itr] * 8);
+    }
+
+
+
+}, 10);
+
 console.log(arr);
